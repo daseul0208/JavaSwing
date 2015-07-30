b2eeaa21e3c98b0e5c162b93eb874691f8ff90dd
평일반 <@ITBANK28-PC>
2015-07-30 15:24:09 +0900

    2015.07.30

---
+++ JavaSwing/src/swing/LottoUI.java
@@ -0,0 +1,19 @@
+package swing;
+// UI : User Interface 사용자 화면
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class LottoUI extends JFrame implements ActionListener{
+	public LottoUI() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
